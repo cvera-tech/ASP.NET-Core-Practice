9259f25aae61fd794a6cf9ab18953ad89be76ab0
jbvera <johnbenedictv@gmail.com>
2019-11-10 18:03:09 -0500

    Updated Movie model
    - Added Rating property
    - Updated Movie views
    - Updated SeedData

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -22,5 +22,7 @@ namespace RazorPagesMovie.Models
 
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        public String Rating { get; set; }
     }
 }
