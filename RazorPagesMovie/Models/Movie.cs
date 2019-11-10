580b1cd55bb290c502a8be9e1b777b1c34794ca3
jbvera <johnbenedictv@gmail.com>
2019-11-10 17:16:32 -0500

    Added search and filtering

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,12 +10,17 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
         public string Title { get; set; }
         
-        // Time data neither required nor displayed.
+        // DataType.Date means that time is neither required nor displayed.
         [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -21,11 +22,41 @@ namespace RazorPagesMovie.Pages.Movies
 
         public IList<Movie> Movie { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public SelectList Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
+            // Genres LINQ query defined
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+            // Search LINQ query defined
+            var movies = from m in _context.Movie 
+                         select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                // Search LINQ query modified
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                // Search LINQ query modified
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+
+            // Run queries!
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
+
             // Task is returned implicitly
-            Movie = await _context.Movie.ToListAsync();
         }
     }
 }
